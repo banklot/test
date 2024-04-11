beebb80e884b4a2a4e60f1edff89be8dd0fc1933
gtahhann <gina.tahhann@gmail.com>
2024-04-11 08:14:44 -0400

    DashboardLogin version 1

--- src/layout/AppSidebarLeft.vue
+++ src/layout/AppSidebarLeft.vue
@@ -9,26 +9,22 @@ const chkCerrarSesion = ref(false);
 
 <template>
   
-  <div class="card"  style="background-color:#F1F1F1" >
+  <div class="card pl-3 pr-3"  style="background-color:#F1F1F1"   >
     
-   <div class="flex flex-column gap-1">
-    <table> <td class="font-medium mr-3 text-sm align-left-center text-color:#A4A4A4"> Iniciar sesión</td>
-            <td class="text-xs text-right  font-bold text-color:#052C65">Crear cuenta</td>   
-    </table>
+   <table calss="flex pt-0 flex flex-row flex-wrap">
+      <td class="flex align-left font-medium  p-1 pr-4 text-sm text-color:#A4A4A4"> Iniciar sesión</td>
+      <td class="text-xs align-text-right  text-color:#052C65">Crear cuenta</td>   
+   </table> 
       <div class="flex justify-content-between align-self-stretch p-0" ><InputText id="username" v-model="userName" aria-describedby="username-help" /></div>
-       <div class="flex justify-content-between align-self-stretch p-0" >
-        <InputText id="password" v-model="pass" />
-       <Button id="width_boton" label="Entrar"/> </div>
+       <div class="flex align-self-stretch p-0 pt-1 pb-1"><InputText id="password" v-model="pass" /><Button  class="flex flex-wrap  align-self-stretch pl-3"  label="Entrar"/> </div>
       <InputGroup>
-    
-      <table>
-      <td class="text-xs text-color:#AFA8A8 font-light"><Checkbox v-model="chkCerrarSesion" :binary="true"/> No cerrar sesión</td>
-      <td class="text-xs text-righ  font-light underline text-color:#AFA8A8">Recordar contraseña</td>
-      </table>
- </InputGroup>
- <div> <Button size="200" icon="pi pi-google" label="Continuar con Google"  severity="secondary" outlined @click="poner_url_google_auth"  rounded/> </div>
+      <table  class="flex flex-row flex-wrap">
+        <td class="flex text-xs pr-3 font-light align-self-stretch font-ligth pr-0 text-color:#A4A4A4"><Checkbox v-model="chkCerrarSesion" :binary="true"/>Cerrar sesión</td>
+        <td class="flex text-xs text-align:right align-self-stretch font-light underline text-color:#AFA8A8">Recordar contraseña</td>   
+     </table> 
 
-  </div>
+      </InputGroup>
+ <div class="flex flex-row flex-wrap"> <Button icon="pi pi-google" label="Continuar con Google"  severity="secondary" outlined @click="poner_url_google_auth"  rounded/> </div>
 </div>
 
 </template>
@@ -38,6 +34,6 @@ const chkCerrarSesion = ref(false);
 }
 
 #width_boton{
-  width:3rem;
+  width:4.5rem;
 }
 </style>  
